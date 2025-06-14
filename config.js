a097f37c5b37659cb539e708d5882974c58d87ac
Dhruv <dhruvstudy77@gmail.com>
2025-06-14 07:46:26 +0530

    Create config.js dummy

---
+++ config.js
@@ -0,0 +1,7 @@
+// API Keys - Replace these with your actual keys
+const config = {
+  HUGGINGFACE_API_KEY: 'YOUR_HUGGING_FACE_API_KEY',
+  GOOGLE_TRANSLATE_API_KEY: 'YOUR_GOOGLE_TRANSLATE_API_KEY'
+};
+
+export default config;
